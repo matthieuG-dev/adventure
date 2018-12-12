1831b4618de533a95c33cf1cd2cc285decf3327b
inalco92 <matthieugros.dev@gmail.com>
2018-12-12 20:12:41 +0100

    project cleaner après suivi avec le prof / fonction readConf

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
-include 'createMap.php';
+include 'readConf.php';
 
-createMap($file, $mapFile);
+//lectrure du le fichier de conf (conf.txt) et stockage
+// de chaque ligne dans un tableau multidimensionnel
+$file = 'conf.txt';
+$conf = readConf($file);
+
+//affichage du tableau multi (entouré des balise 
+//<pre></pre> pour une meilleure lecture)
+echo "<pre>";
+print_r($conf);
+echo "</pre>";
+
 ?>
--- trash.php
+++
@@ -1,9 +0,0 @@
-//lire le contenu d'un fichier caractère par caractère: <br/><br/>";
-// $fp = fopen ("test.txt", "r");
-// $i = 1;
-// while (!feof($fp)) {
-//     echo "ligne " . $i . " : " . fgetc ($fp) . "<br/>";
-//     $i++;
-// }
-// fclose ($fp);
-// echo "<br/><br/>";
